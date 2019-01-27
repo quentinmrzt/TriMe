19b0682c439ae755709dd7c7761f07f22eea3791
quentinmrzt <quentin.morizot@outlook.fr>
2019-01-27 11:40:03 +0100

    Modif gestion dessin

--- src/model/CreationImageAvecDessinChemins.java
+++ src/model/CreationImageAvecDessinChemins.java
@@ -6,13 +6,23 @@ public class CreationImageAvecDessinChemins {
 
 	public static Image executer(Image image, Historique historique) {
 		int[][] tableau = new int[image.getLargeur()][image.getHauteur()];
-		
+
+		for (int y=0; y<image.getHauteur(); y++) {
+			for (int x=0; x<image.getLargeur(); x++) {
+				if(true) {
+					tableau[x][y] = image.getPixel(x, y);
+				} else {
+					tableau[historique.positionX(iteration, y)][y] = Color.RED.getRGB();
+				}
+			}
+		}
+
 		copie(image, tableau);
 		dessin(tableau, historique);
-		
+
 		return new Image(image.getNom(), image.getExtension(), tableau);
 	}
-	
+
 	private static void copie(Image image, int[][] tableau) {
 		for (int y=0; y<image.getHauteur(); y++) {
 			for (int x=0; x<image.getLargeur(); x++) {
@@ -20,7 +30,7 @@ public class CreationImageAvecDessinChemins {
 			}
 		}
 	}
-	
+
 	private static void dessin(int[][] tableau, Historique historique) {
 		for (int iteration=0; iteration<historique.getTaille(); iteration++) {
 			for (int y=0; y<historique.getHauteur(); y++) {
